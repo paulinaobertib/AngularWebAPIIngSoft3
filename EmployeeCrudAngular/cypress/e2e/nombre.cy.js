115899b418e48f555debee81b327aa667d77611f
Pau Oberti <paulinaobertibusso@gmail.com>
2024-10-08 17:06:02 -0300

    actualizacion bd

--- EmployeeCrudAngular/cypress/e2e/nombre.cy.js
+++ EmployeeCrudAngular/cypress/e2e/nombre.cy.js
@@ -1,8 +1,18 @@
 describe('Validación: Nombre contiene números', () => {
     it('Debe mostrar un mensaje de error cuando el nombre contiene números', () => {
-        cy.visit('http://localhost:4200/addemployee/');
+        const baseUrl = Cypress.env('baseUrl'); // Obtener la URL desde las variables de entorno
+
+        // Verificar si baseUrl está definido
+        if (!baseUrl) {
+            throw new Error('Error: baseUrl is not defined. Please check your environment variables.');
+        }
+
+        cy.visit(baseUrl + '/addemployee/'); // Usar la URL construida
         cy.get('input[name="name"]').type('Paulina05'); 
         cy.get('.btn').click();
+        
+        // Comprobar que se muestre el mensaje de error
         cy.get('.toast-message').should('contain.text', 'El nombre no puede tener números.');
     });
 });
+
